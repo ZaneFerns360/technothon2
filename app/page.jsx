24bf21b530025ea45c4e815a950eecee19283b41
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-16 16:18:43 +0530

    getting started

---
+++ app/page.jsx
@@ -0,0 +1,79 @@
+import Image from 'next/image'
+import MultiLayerParallax from '@components/MultiLayerParallax'
+
+export default function Home() {
+  return (
+    <div>
+      <MultiLayerParallax />
+      <div className="w-full bg-[#06141D]">
+        <div className="mx-auto max-w-lg space-y-4 py-24 text-neutral-300">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+        </div>
+      </div>
+    </div>
+    // <div className="relative min-h-screen bg-[#81D0D4]">
+    //   <div className="absolute inset-0 bg-[#81D0D4]">
+    //     <Image
+    //       alt="main-img"
+    //       width={1920}
+    //       height={1080}
+    //       src="/assets/pnggg.jpg"
+    //       className="opacity-50"
+    //     ></Image>
+    //   </div>
+    //   <div className="absolute inset-0 flex items-center justify-center">
+    //     <Image alt="GG" width={500} height={500} src="/assets/GG.png"></Image>
+    //   </div>
+    //   <div className="absolute bottom-0 w-full">
+    //     <Image
+    //       alt="sea-link"
+    //       width={1920}
+    //       height={1080}
+    //       src="/assets/sea-link.png"
+    //       className=""
+    //     ></Image>
+    //   </div>
+    // </div>
+  )
+}
---
+++ components/MultiLayerParallax.jsx
@@ -0,0 +1,45 @@
+'use client'
+import Image from 'next/image'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import React, { useRef } from 'react'
+
+export default function MultiLayerParallax() {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start start', 'end start'],
+  })
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ['0%', '100%'])
+  const textY = useTransform(scrollYProgress, [0, 1], ['0%', '200%'])
+
+  return (
+    <div
+      ref={ref}
+      className="relative grid h-screen w-full place-items-center overflow-hidden"
+    >
+      <motion.div style={{ y: textY }} className="absolute inset-0 z-30">
+        <div className="z-21 absolute inset-0 flex items-center justify-center">
+          <Image alt="GG" width={400} height={400} src="/assets/GG.png"></Image>
+        </div>
+      </motion.div>
+
+      <motion.div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `linear-gradient(rgba(129, 208, 212, 0.5), rgba(129, 208, 212, 0.5)), url(/assets/pnggg.jpg)`,
+          backgroundPosition: 'bottom',
+          backgroundSize: 'cover',
+          y: backgroundY,
+        }}
+      />
+
+      <div
+        className="absolute inset-0 z-20 w-full bg-contain bg-no-repeat"
+        style={{
+          backgroundImage: `url(/assets/sea-link.svg)`,
+          backgroundPosition: 'bottom',
+        }}
+      />
+    </div>
+  )
+}
