022c0985d0dde080d5e4d64e56300cb973872bb0
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-18 00:11:55 +0530

    minor changes

--- components/About.jsx
+++ components/About.jsx
@@ -2,25 +2,24 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="mx-auto flex h-screen w-4/6 flex-col items-center justify-center px-5 text-center">
-      <h2 className="mb-5 text-3xl font-bold sm:text-7xl">
+    <div className="mx-auto flex h-screen w-4/6 flex-col items-center justify-center px-5 text-center max-sm:pt-36">
+      <h2 className="mb-5 text-3xl font-bold max-sm:pt-64 sm:text-7xl">
         About Technothon 2024⚡
       </h2>
       <div className="leading-relaxed">
-        {' '}
         <p className="mb-5 text-2xl sm:text-3xl">
           {
-            'Mumbai polytechnic students, gear up for Technothon 2024! A 24-hour hackathon packed with excitement, surprises, and a chance to push your creativity to new heights.'
+            'Mumbai polytechnic students, get ready for Technothon 2024! A 24-hour hackathon full of excitement and creativity.'
           }
         </p>
         <p className="mb-5 text-2xl sm:text-3xl">
           {
-            'Technothon is your platform to shine as a developer. Build from scratch, compete with peers, and unlock your innovator within. Ready for a skill-testing challenge?'
+            'Technothon is your chance to shine as a developer. Build from scratch, compete, and unlock your potential. Are you up for the challenge?'
           }
         </p>
         <p className="text-2xl sm:text-3xl">
           {
-            'This year features Blind Bet Registration! Register and discover your challenges on the spot. Embark on this thrilling journey at Technothon 2024, the most exciting hackathon yet!'
+            'This year, we introduce Blind Bet Registration! Register, discover your challenges on the spot, and embark on an exciting journey at Technothon 2024.'
           }
         </p>
       </div>
