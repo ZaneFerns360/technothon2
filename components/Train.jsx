f563158a4f8eae3e845ad914b2a7f0329b0484da
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-19 13:55:17 +0530

    some changes

---
+++ components/Train.jsx
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+const Train = () => {
+  return (
+    <div className="w-full flex-row bg-[#06141d]">
+      <motion.div
+        style={{
+          position: 'absolute',
+          right: 0,
+          backgroundColor: '#06141d',
+          paddingTop: '20px',
+        }}
+        animate={{ x: '-100vw' }}
+        transition={{ duration: 5, repeat: Infinity, ease: 'linear' }}
+      >
+        <div className="w-full">
+          {' '}
+          <Image alt="train" height={200} width={200} src="/assets/train.png" />
+        </div>
+      </motion.div>
+    </div>
+  )
+}
+
+export default Train
